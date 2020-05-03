2b29adc7b44e921c8e7bcf1a11f7f0493dcfacdb
manikantanandi9700 <manikantanandi9700@gmail.com>
2020-05-03 20:39:22 +0530

    mom work done

---
+++ src/org/xerox/A.java
@@ -0,0 +1,16 @@
+package org.xerox;
+
+public class A {
+	public void bank() {
+		System.out.println("karnataka");
+		
+		
+	}
+	public void company() {
+		System.out.println("HCL");
+		
+	}
+public void Id() {
+	System.out.println("9700");
+}
+}
---
+++ src/org/xerox/B.java
@@ -0,0 +1,17 @@
+package org.xerox;
+
+public class B {	
+	public void bank() {
+	System.out.println("canara");
+	
+	
+}
+public void company() {
+	System.out.println("TCS");
+	
+}
+public void Id() {
+System.out.println("9246");
+}
+
+}
